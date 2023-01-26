1d593662a793ed05f6b9f2b85174c2c16be6cff6
jliu0140 <122562868+jliu0140@users.noreply.github.com>
2023-01-26 10:40:33 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,25 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedLonger() {
+    int[] input1 = {1,2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseLonger() {
+    int[] input1 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input1);
+  }
+
+  @Test
+  public void testReverseLonger2() {
+    int[] input1 = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input1);
   }
 }
